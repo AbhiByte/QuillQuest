3dacfad6d9ef167d8e94f55c90ac4b31de3e51a2
Abhinav Ramesh <dbossgames20@gmail.com>
2023-08-31 10:47:12 -0400

    install react router dom

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="/">
             Navbar
           </a>
           <button
@@ -21,18 +21,18 @@ export default function Navbar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-              <a className="nav-link active" aria-current="page" href="#">
+              <a className="nav-link active" aria-current="page" href="/">
                 Home
               </a>
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/">
                 Features
               </a>
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/">
                 Pricing
               </a>
               <a
                 className="nav-link disabled"
-                href="#"
+                href="/"
                 tabindex="-1"
                 aria-disabled="true"
               >
